94928a93f2b452c08af72f4a0a417a25eff2c0fe
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-08-30 14:55:17 -0300

    Cria pipe para formatar cpf dos dependentes do usuário

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CpfPipe } from './cpf.pipe';
 import { MaritalStatusPipe } from './marital-status.pipe';
 
 @NgModule({
-  declarations: [MaritalStatusPipe],
-  exports: [MaritalStatusPipe],
+  declarations: [MaritalStatusPipe, CpfPipe],
+  exports: [MaritalStatusPipe, CpfPipe],
 })
 export class PipesModule {}
