b8c3ae016a503f30aa5fe6ab2136e3b402f33072
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-09-26 14:11:55 -0300

    Cria pipe de máscara de telefone

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CpfPipe } from './cpf.pipe';
 import { MaritalStatusPipe } from './marital-status.pipe';
+import { PhoneMaskPipe } from './phone-mask.pipe';
 
 @NgModule({
-  declarations: [MaritalStatusPipe, CpfPipe],
-  exports: [MaritalStatusPipe, CpfPipe],
+  declarations: [MaritalStatusPipe, CpfPipe, PhoneMaskPipe],
+  exports: [MaritalStatusPipe, CpfPipe, PhoneMaskPipe],
 })
 export class PipesModule {}
