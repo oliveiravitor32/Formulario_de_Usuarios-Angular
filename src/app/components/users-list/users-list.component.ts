3eff370f07bb65e35d29dae1e75b0dae119727e7
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-09-27 13:15:11 -0300

    Implementa lógica para desabilitar seleção de usuário enquando o modo de edição estiver habilitado

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -11,10 +11,15 @@ export class UsersListComponent {
 
   @Input({ required: true }) usersList: UsersListResponse =
     {} as UsersListResponse;
+  @Input({ required: true }) isInEditMode: boolean = false;
 
-  @Output('onUserSelected') onUserSelectedEmitt = new EventEmitter<number>();
+  @Output('onUserSelected')
+  onUserSelectedEmitt = new EventEmitter<number>();
 
   onUserSelected(userIndex: number) {
+    // Não deixa selecionar usuário se o modo de edição estiver ativo!
+    if (this.isInEditMode) return;
+
     this.userSelectedIndex = userIndex;
     this.onUserSelectedEmitt.emit(userIndex);
   }
